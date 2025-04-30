184ff1e8609fca6097964c8a1a07603b5f230d1a
Liam Lennon-Flynn <82933053+imliamflynn@users.noreply.github.com>
2025-04-30 14:36:18 +1200

    Code clarity.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}])
+  const [backendData, setBackendData] = useState([{}]);
 
   useEffect(() => {
     fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+      response => response.json()).then(data => {
+        setBackendData(data);
+      });
+  }, []);
 
   return (
     <div>
@@ -26,7 +23,7 @@ function App() {
       )}
 
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ "users": ["userOne", "userTwo", "userThree", "userFour"] })
-})
+    res.json({ "users": ["userOne", "userTwo", "userThree", "userFour"] });
+});
 
-app.listen(2000, () => { console.log("Server started on port 2000") })
+app.listen(2000, () => { console.log("Server started on port 2000") });
